6b3eff0375c375d6747c6719d80e66aea285dcc9
Rivki Renaldi <167266835+rvrnldi@users.noreply.github.com>
2024-10-20 07:43:04 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme:{
+        extend:{
+            gridTemplateColumns:{
+                'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            animation:{
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors:{
+                lightHover: '#c1c1c2',
+                darkHover: '#c2a004a',
+                darkTheme: '#11001F'
+            },
+            boxShadow:{
+                'black': '4px 4px 0 #000',
+                'white': '4px 4px 0 #fff',
+        }
+    }
+},
+        darkMode: 'selector'
+}
